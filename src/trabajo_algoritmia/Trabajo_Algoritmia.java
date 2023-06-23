bd1e21003ae60d69f41fefd0af9b825f8467cb7d
SilencedSign87 <134737468+SilencedSign87@users.noreply.github.com>
2023-06-23 22:59:09 +0000

    hice el control, añadí que solo se puedan ingresar enteros en las filas y columnas, y que solo pueda ingresar números naturales en casa slot del array

--- src/trabajo_algoritmia/Trabajo_Algoritmia.java
+++ src/trabajo_algoritmia/Trabajo_Algoritmia.java
@@ -7,27 +7,69 @@ import java.util.*;
 public class Trabajo_Algoritmia {
 
     public static void main(String[] args) {
-        
+        //variables
+        int filas, columnas;
         Scanner scanner = new Scanner(System.in);
+        //entrada
 
-        System.out.print("Ingrese el número de filas: ");
-        int filas = scanner.nextInt();
-        System.out.print("Ingrese el número de columnas: ");
-        int columnas = scanner.nextInt();
+        //filas
+        do {
+            System.out.print("Ingrese el número de filas: ");
 
+            if (scanner.hasNextInt()) {
+                filas = scanner.nextInt();
+
+                if (filas < 0) {
+                    System.out.println("El número de filas debe ser mayor que 0");
+                }
+            } else {
+                System.out.println("Debe ingresar un número entero.");
+                scanner.next(); // Consumir entrada inválida para evitar un bucle infinito
+                filas = -1; // Asignar un valor inválido para que se repita el bucle
+            }
+        } while (filas < 0);
+
+        //columnas
+        do{
+            System.out.print("Ingrese el número de columnas: ");
+            
+            if(scanner.hasNextInt()){
+                columnas = scanner.nextInt();
+
+                if(columnas < 0){
+                    System.out.println("El número de filas debe ser mayor que 0");
+                }
+            }else{
+                System.out.println("Debe ingresar un número entero.");
+                scanner.next(); //consumir entrada inválida para evitar un bucle infinito
+                columnas=-1; // asignar un valor inválido para que se repita el bucle
+            }
+        }while(columnas < 0);
+
+         //creación del array
         int[][] matriz = new int[filas][columnas];
 
+        //entrada de elementos del array
         System.out.println("Ingrese los elementos de la matriz:");
 
         // Llenar la matriz con los valores ingresados por el usuario
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                System.out.println("ingrese el elemento: a("+i+";"+j+")"); //añadí un indicador de elementos para que el usuario pueda saber que esta ingresando
-                matriz[i][j] = scanner.nextInt();
+                do {
+                    System.out.println("Ingrese el elemento en la posición a(" + (i + 1) + ";" + (j + 1) + "):");
+
+                    if (scanner.hasNextInt()) {
+                        matriz[i][j] = scanner.nextInt();
+                        break; // Salir del bucle si se ingresó un número entero válido
+                    } else {
+                        System.out.println("Debe ingresar un número entero.");
+                        scanner.next(); // Consumir entrada inválida para evitar un bucle infinito
+                    }
+                } while (true);
             }
         }
-
         scanner.close();
+        //salida
 
         if (esMatrizNula(matriz)) {
             System.out.println("La matriz es nula.");
@@ -41,6 +83,17 @@ public class Trabajo_Algoritmia {
             System.out.println("La matriz es una columna.");
         } else {
             System.out.println("La matriz no cumple con ningún tipo específico.");
+        }
+        //representación gráfica de la matriz (prueba)
+        int aux;
+        aux=0;
+         for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                
+                System.out.print(matriz[i][j] + " "); //Impresión de cada elemento y un espacio
+                
+            }
+            System.out.println(); // Salto de línea para crear una nueva fila
         }
     }
 
